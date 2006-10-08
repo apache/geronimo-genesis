6c0e2a554302a36108aaa31c9d9c230831202cc9
Jason Dillon <jdillon@apache.org>
2006-10-08 23:59:07 +0000

    Don't need factory or resolver anymore
    git-svn-id: https://svn.apache.org/repos/asf/geronimo/genesis/trunk@454239 13f79535-47bb-0310-9956-ffa450edef68

--- plugins/script-maven-plugin/src/main/java/org/apache/geronimo/genesis/plugins/script/GroovyMojo.java
+++ plugins/script-maven-plugin/src/main/java/org/apache/geronimo/genesis/plugins/script/GroovyMojo.java
@@ -31,8 +31,6 @@ import java.util.List;
 import java.util.ArrayList;
 
 import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.artifact.factory.ArtifactFactory;
-import org.apache.maven.artifact.resolver.ArtifactResolver;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
@@ -86,28 +84,6 @@ public class GroovyMojo
 
     protected MavenProject getProject() {
         return project;
-    }
-
-    /**
-     * @component
-     * @required
-     * @readonly
-     */
-    private ArtifactFactory artifactFactory = null;
-
-    protected ArtifactFactory getArtifactFactory() {
-        return artifactFactory;
-    }
-
-    /**
-     * @component
-     * @required
-     * @readonly
-     */
-    private ArtifactResolver artifactResolver = null;
-
-    protected ArtifactResolver getArtifactResolver() {
-        return artifactResolver;
     }
 
     /**
